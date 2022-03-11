91096d21883c435771cc05ddadf3d01996c6dce8
Tim Hobson <thobson@turing.ac.uk>
2022-03-11 13:18:59 +0000

    Edit comments

--- s2fr/tests/testthat/test-gross_stock.R
+++ s2fr/tests/testthat/test-gross_stock.R
@@ -6,16 +6,16 @@ test_that("the gross_stock function works", {
   expect_equal(gross_stock(0), 0)
   expect_equal(gross_stock(1), 5000000000)
 
-  # The last 50 BTC coinbase.
+  # The last 50 BTC subsidy.
   expect_equal(gross_stock(209999), 5000000000*209999)
 
-  # The first 25 BTC coinbase.
+  # The first 25 BTC subsidy.
   expect_equal(gross_stock(210000), 5000000000*209999 + 2500000000)
 
-  # The last 25 BTC coinbase.
+  # The last 25 BTC subsidy.
   expect_equal(gross_stock(419999), 5000000000*209999 + 2500000000*210000)
 
-  # first 12.5 BTC coinbase.
+  # first 12.5 BTC subsidy.
   expect_equal(gross_stock(420000), 5000000000*209999 + 2500000000*210000 + 1250000000)
 
 })
